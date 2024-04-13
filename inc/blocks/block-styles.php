af68a2e8179d660df2ba1ec10317962624d0468b
Pea <pea@misfist.com>
2024-04-13 01:53:48 -0400

    Disable ogtags

--- inc/blocks/block-styles.php
+++ inc/blocks/block-styles.php
@@ -20,5 +20,21 @@ function register_block_styles() : void {
             'label'        => __( 'Fancy', 'tvlr' ),
         )
 	);
+
+	register_block_style(
+		'core/paragraph',
+		array(
+            'name'         => 'intro',
+            'label'        => __( 'Intro', 'tvlr' ),
+        )
+	);
+
+	register_block_style(
+		'core/gallery',
+		array(
+            'name'         => 'logos',
+            'label'        => __( 'Logo Grid', 'tvlr' ),
+        )
+	);
 }
 add_action( 'init', __NAMESPACE__ . '\register_block_styles' );

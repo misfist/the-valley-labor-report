cb71ae3bd69a8d056019da43ef2b7437bef36c87
Pea <pea@misfist.com>
2024-04-19 01:15:58 -0400

    Add block binding

--- inc/blocks/block-bindings.php
+++ inc/blocks/block-bindings.php
@@ -7,17 +7,33 @@
 
 namespace TVLR;
 
-
-function register_block_bindings() {
+/**
+ * Register block binding source
+ * 
+ * @see https://developer.wordpress.org/reference/functions/register_block_bindings_source/
+ *
+ * @return void
+ */
+function register_block_bindings() : void {
 	register_block_bindings_source( __NAMESPACE__ . '/featured-image', array(
 		'label'              => __( 'Featured Image', 'tvlr' ),
 		'get_value_callback' => __NAMESPACE__ . '\get_featured_image_url',
 		'uses_context'       => [ 'postId' ]
 	) );
+
+	register_block_bindings_source( __NAMESPACE__ . '/copyright', array(
+		'label'              => __( 'Copyright', 'tvlr' ),
+		'get_value_callback' => __NAMESPACE__ . '\get_copyright',
+	) );
 }
 add_action( 'init', __NAMESPACE__ . '\register_block_bindings' );
 
-
+/**
+ * Callback
+ *
+ * @param  array $source_args
+ * @return mixed
+ */
 function get_featured_image_url( $source_args ) {
 	global $post;
 	if( ! $post || ! is_a( $post, '\WP_Post' ) ) {
@@ -27,4 +43,13 @@ function get_featured_image_url( $source_args ) {
 	$size = ( isset( $source_args['size'] ) ) ? esc_attr( $size ) : 'thumbnail';
 
 	return get_the_post_thumbnail_url( $post_id, $size );
+}
+
+/**
+ * Return copyright text
+ *
+ * @return string
+ */
+function get_copyright() : string {
+	return '&copy;' . date( 'Y' );
 }
--- patterns/footer.php
+++ patterns/footer.php
@@ -5,9 +5,23 @@
  * Categories: hidden
  * Inserter: no
  */
+$copyright_info = esc_html__( ' Copyright &copy; ', 'tvlr' ) . esc_attr( gmdate( 'Y' ) ) . esc_html__( '. All Rights Reserved.', 'tvlr' );
 ?>
 <!-- wp:group {"tagName":"div","className":"site-footer__inner","layout":{"type":"default","wideSize":"100vw"},"metadata":{"name":"Site Footer"}} -->
 <div class="wp-block-group site-footer__inner">
 	<!-- wp:site-logo {"width":250,"shouldSyncIcon":true} /-->
+
+	<!-- wp:paragraph {
+		"metadata":{
+			"bindings":{
+				"content":{
+					"source":"TVLR/copyright"
+				}
+			}
+		},
+		"className": "copyright-info bound"
+	} -->
+	<p class="copyright-info"><?php echo $copyright_info; ?></p>
+	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
